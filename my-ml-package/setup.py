2717e6927162c320809febf1392b3020f22f0de3
dkarpov.legal <dkarpov.legal@gmail.com>
2025-01-18 22:16:15 +0300

    Publish Packages

---
+++ my-ml-package/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "My first ML package"
+LONG_DESCRIPTION = "My first ML package on credit defaults"
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="my-ml-package",
+    version=VERSION,
+    author="Danil Karpov",
+    author_email="<youremail@email.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=["python", "first package"],
+    classifiers=["ML"],
+)
