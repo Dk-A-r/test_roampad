33e6b6d76e157b3a472b1f6718d349b1b387c161
dkarpov.legal <dkarpov.legal@gmail.com>
2025-01-19 19:03:50 +0300

    Some new commit

--- mymlpackage/setup.py
+++ mymlpackage/setup.py
@@ -7,14 +7,21 @@ LONG_DESCRIPTION = "My first ML package on credit defaults"
 # Setting up
 setup(
     # the name must match the folder name 'verysimplemodule'
-    name="my-ml-package",
+    name="mymlpackage",
     version=VERSION,
     author="Danil Karpov",
     author_email="<youremail@email.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],  # add any additional packages that
+    install_requires=[
+        "pandas",
+        "seaborn",
+        "matplotlib",
+        "nbconvert",
+        "jupyter",
+        "nbformat",
+    ],  # add any additional packages that
     # needs to be installed
     keywords=["python", "first package"],
     classifiers=["ML"],
